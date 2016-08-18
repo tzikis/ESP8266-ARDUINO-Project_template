18469bab20f3157fb63dd08762109cadc4f982bc
Vasilis Georgitzikis <tzikis@gmail.com>
2016-08-18 15:13:08 +0300

    Delete bump-version.sh

--- bump-version.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-version=$(cat /tmp/version)
-echo "Old version: $version"
-((version++))
-echo "New version: $version" && \
-echo $version > /tmp/version$version > /tmp/version && \
-echo "Successfuly set new version to $version"
